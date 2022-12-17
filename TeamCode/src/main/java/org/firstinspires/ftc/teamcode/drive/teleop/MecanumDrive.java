1b9557d92e1a3340fba84bda1e001670209d9e87
adityasaran <epicplayzquestions@gmail.com>
2022-12-16 21:27:43 -0400

    Moving stuff around

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teleop/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teleop/MecanumDrive.java
@@ -10,7 +10,6 @@ import com.acmerobotics.dashboard.config.Config;
 public class MecanumDrive extends OpMode
 {
     // Drivetrain motors
-    // test
     private DcMotorEx LF = null;
     private DcMotorEx RF = null;
     private DcMotorEx RB = null;
