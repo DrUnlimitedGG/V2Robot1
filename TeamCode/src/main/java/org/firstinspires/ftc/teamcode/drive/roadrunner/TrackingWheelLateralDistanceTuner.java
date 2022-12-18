b148a38fecad4fce2b1f478c3b7137dd5b4b4394
adityasaran <epicplayzquestions@gmail.com>
2022-12-17 20:19:30 -0400

    Disabled a bunch of OpModes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/roadrunner/TrackingWheelLateralDistanceTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/roadrunner/TrackingWheelLateralDistanceTuner.java
@@ -61,6 +61,7 @@ import org.firstinspires.ftc.teamcode.drive.opmodes.SampleMecanumDrive;
  * precision. The heading should still line up.
  */
 @Config
+@Disabled
 @TeleOp(group = "drive")
 public class TrackingWheelLateralDistanceTuner extends LinearOpMode {
     public static int NUM_TURNS = 10;
